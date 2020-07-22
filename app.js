819b061d072e7dfc0f996330304197603c15eca6
luiseduardovb <le_velasquez@yahoo.com>
2020-07-22 16:07:16 +0300

    created app.delete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 //Data
-const tasks = require("./tasks");
+let tasks = require("./tasks");
 
 //Express App Instance
 const app = express();
@@ -11,6 +11,17 @@ app.use(cors());
 
 app.get("/tasks", (req, res) => {
   res.json(tasks);
+});
+
+app.delete("/tasks/:taskId", (req, res) => {
+  const { taskId } = req.params;
+  const foundTask = tasks.find((task) => task.id !== +taskId);
+  if (foundTask) {
+    tasks = tasks.filter((task) => task.id !== +foundTask);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Task not found" });
+  }
 });
 
 app.listen(8000, () => {
